800b1e7bfba556f5f681b8b9456077ddd4cda73f
Felipe Neves <felipeneves@sistemafieg.org.br>
2020-06-09 14:41:13 -0300

    Criação de validação para o formulário

--- src/app/models/tipo.ts
+++ src/app/models/tipo.ts
@@ -1,4 +1,9 @@
-export interface Tipo {
-    id: number,
+export class Tipo {
+    id: number;
     nome: string;
+
+    constructor(id: number, nome: string) {
+        this.id = id;
+        this.nome = nome;
+    }
 }
